9022d95ac8529c0a8d8f388216bf86c10fabf384
RobinSherbataky <52579321+RobinSherbataky@users.noreply.github.com>
2019-10-06 20:21:26 +0600

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title> Home </title>
+       <link rel = "stylesheet" type = "text/css" href = "Style.css">
+      <link rel = "stylesheet" type = "text/css" 
+      href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+
+<nav>
+    <!-- <img src = "HostelManagementLogo02.jpg" width = "200" height = "80">-->
+    <ul>
+    
+     <li><a href="AdminLoginRegistrationHTML.php">ADMIN</a></li>
+     <li><a href="StudentLoginRegistrationHTML.php">STUDENT</a></li>
+     <li><a href="SearchStudentInfo.html">ABOUT US</a></li>
+    </ul>
+   </nav>
+  
+
+
+</body>
+
+
+</html>
